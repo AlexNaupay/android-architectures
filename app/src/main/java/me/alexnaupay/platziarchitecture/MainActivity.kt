fcad502889639e46f77782a71c044d372f8cbf22
Alex Naupay <alexnaupay@gmail.com>
2023-04-09 13:39:39 -0500

    Update libraries, run app

--- app/src/main/java/me/alexnaupay/platziarchitecture/MainActivity.kt
+++ app/src/main/java/me/alexnaupay/platziarchitecture/MainActivity.kt
@@ -2,10 +2,57 @@ package me.alexnaupay.platziarchitecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import me.alexnaupay.platziarchitecture.entities.Coupon
+import me.alexnaupay.platziarchitecture.model.ApiAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //VIEW
+        val rvCoupons: RecyclerView = findViewById(R.id.rvCoupons) //UI
+        rvCoupons.layoutManager = LinearLayoutManager(this)
+        val coupons = ArrayList<Coupon>()
+        //VIEW
+
+
+        //CONTROLLER
+        val apiAdapter = ApiAdapter()
+        val apiService = apiAdapter.getClientService()
+        val call = apiService.getCoupons()
+
+        call.enqueue(object : Callback<JsonObject> {
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                Log.e("ERROR: ", t.message.toString())
+                t.stackTrace
+            }
+
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                val offersJsonArray = response.body()?.getAsJsonArray("offers")
+                offersJsonArray?.forEach { jsonElement: JsonElement ->
+                    var jsonObject = jsonElement.asJsonObject
+                    var coupon = Coupon(jsonObject)
+                    if (coupon.image_url.isNotBlank()){
+                        coupons.add(coupon)
+                    }
+
+                }
+                //VIEW
+                rvCoupons.adapter = RecyclerCouponsAdapter(coupons, R.layout.card_coupon)
+                //VIEW
+            }
+
+
+        })
+        //CONTROLLER
     }
 }
---
+++ app/src/main/java/me/alexnaupay/platziarchitecture/model/ApiAdapter.kt
@@ -0,0 +1,40 @@
+package me.alexnaupay.platziarchitecture.model
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiAdapter {
+    //val apiKey = "69d1837829128f9565368ca704c63207"
+    val apiKey = "fe253b2b14e3b891c7310d0115a5939d"
+    val urlApi = "https://feed.linkmydeals.com/"
+
+    fun getClientService(): ApiService {
+        val authInterceptor = Interceptor { chain ->
+            val url = chain.request().url().newBuilder()
+                .addQueryParameter("API_KEY", apiKey)
+                .addQueryParameter("format", "json")
+                .build()
+
+            val newRequest = chain.request()
+                .newBuilder()
+                .url(url)
+                .build()
+
+            chain.proceed(newRequest)
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(authInterceptor).build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(urlApi)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return  retrofit.create(ApiService::class.java)
+    }
+
+}
